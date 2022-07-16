0e00316caa701a324ef758d090931c1c9e57a86f
Alain Hélaïli <alain@carbonhub.io>
2022-07-16 06:44:37 -0400

    use only actions kit

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const { Octokit } = require("@octokit/rest");
-const { createAppAuth } = require("@octokit/auth-app");
+const core = require('@actions/core')
+const github = require('@actions/github')
+// const { Octokit } = require('@octokit/rest')
+// const { createAppAuth } = require('@octokit/auth-app')
 
 
 try {
   console.log(`Retrieving installations for app ${core.getInput('app-id')}`)
-  
-  const appOctokit = new Octokit({
-    authStrategy: createAppAuth,
+
+  const octokit = new github.GitHub({
+    authStrategy: github.createAppAuth,
     auth: {
       appId: core.getInput('app-id'),
       privateKey: core.getInput('private-key'),
     },
   })
 
-  appOctokit.rest.apps.listInstallations().then(res => {
+  octokit.rest.apps.listInstallations().then(res => {
     console.log(`Found installations : ${JSON.stringify(res.data.installations)}`)
     core.setOutput('installations', res.data.installations)
   }).catch(err => {
