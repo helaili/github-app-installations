fa44eec5072889184db7c5180a088f2014fd463b
Alain Hélaïli <alain@carbonhub.io>
2022-07-16 06:55:20 -0400

    get the octokit

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+import { getOctokitOptions } from '@actions/github/lib/utils'
+
 // const { Octokit } = require('@octokit/rest')
 // const { createAppAuth } = require('@octokit/auth-app')
 
 
 try {
   console.log(`Retrieving installations for app ${core.getInput('app-id')}`)
-
-  const octokit = new github.GitHub({
+  const octokit = github.getOctokit(getOctokitOptions({
     authStrategy: github.createAppAuth,
     auth: {
       appId: core.getInput('app-id'),
       privateKey: core.getInput('private-key'),
     },
-  })
+  }))
+
 
   octokit.rest.apps.listInstallations().then(res => {
     console.log(`Found installations : ${JSON.stringify(res.data.installations)}`)
